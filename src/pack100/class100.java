be60baa423212d58e4f820169d3929adb1d14be9
vaishgithubvdj <vaishjambhule@gmail.com>
2023-11-09 20:18:31 +0530

    commit changes

---
+++ src/pack100/class100.java
@@ -0,0 +1,10 @@
+package pack100;
+
+public class class100 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/package1/class1.java
@@ -0,0 +1,11 @@
+package package1;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		
+		int t = 89;
+		
+		System.out.println(t);
+    }
+}
